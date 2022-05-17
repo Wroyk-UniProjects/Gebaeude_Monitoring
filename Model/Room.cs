f9c62398d7f12c1df533445f4b437f1ea1bd7449
Rudolf Baun <rubait01@hs-esslingen.de>
2022-05-17 18:05:18 +0200

    RoomService implementet

---
+++ Model/Room.cs
@@ -0,0 +1,15 @@
+﻿namespace Building_Monitoring_WebApp.Model
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Individual { get; set; }
+        public string Status { get; set; }
+        public double Hum { get; set; }
+        public double Temp { get; set; }
+        public double TargetHum { get; set; }
+        public double TargetTemp { get; set; }
+        public string TempImageURL { get; set; }
+    }
+}
