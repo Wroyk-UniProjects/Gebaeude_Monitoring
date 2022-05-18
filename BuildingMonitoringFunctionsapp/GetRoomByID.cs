8c36abfbcccbfb5879e5f31febd180d2215b82b3
robertfeo <rofeit00@hs-esslingen.de>
2022-05-18 20:57:32 +0200

    PSWTM-30: Kleine Änderungen in GetRoomByID

--- BuildingMonitoringFunctionsapp/GetRoomByID.cs
+++ BuildingMonitoringFunctionsapp/GetRoomByID.cs
@@ -14,14 +14,13 @@ namespace BuildingMonitoringFunctionsapp
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "rooms/{iD}")]
             HttpRequest req,
-             [Sql("select r.[id], m.[date], m.[temp], m.[hum] , r.[name], r.[id] as 'roomId', r.[name], r.[individual], 'ok' as status, rc.[targetTemp], rc.[targetHum] from measurements m "+
+            [Sql("select r.[id], m.[date], m.[temp], m.[hum] , r.[name], r.[id] as 'roomId', r.[name], r.[individual], 'ok' as status, rc.[targetTemp], rc.[targetHum] from measurements m "+
                   "join rooms r on m.roomId=r.id join roomConfig rc on r.id=rc.roomId where date in (select max(date) from measurements group by roomId)" +
                   "and r.[id] = @ID",
                 CommandType = System.Data.CommandType.Text,
                 Parameters = "@ID={iD}",
                 ConnectionStringSetting = "sqlconnectionstring")]
-
-        IEnumerable<RoomDetail>room)
+            IEnumerable<RoomDetail> room)
         {
             return new OkObjectResult(room.FirstOrDefault());
         }
