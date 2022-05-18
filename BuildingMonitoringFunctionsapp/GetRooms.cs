54be5d21aed4d9869c415a01166a7e6f2461b17f
robertfeo <rofeit00@hs-esslingen.de>
2022-05-18 21:15:00 +0200

    PSWTM-31: RoomConfig in GetRooms um den Status zu bestimmen

--- BuildingMonitoringFunctionsapp/GetRooms.cs
+++ BuildingMonitoringFunctionsapp/GetRooms.cs
@@ -3,6 +3,8 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
+using System;
+using System.Data.SqlClient;
 
 namespace BuildingMonitoringFunctionsapp
 {
@@ -12,15 +14,46 @@ namespace BuildingMonitoringFunctionsapp
         public static IActionResult Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "rooms")]
         HttpRequest req,
-        [Sql("select r.[id], m.[date], m.[temp], m.[hum] , r.[name], r.[id] as 'roomId', r.[name], r.[individual], 'ok' as status, rc.[targetTemp], rc.[targetHum] from measurements m "+
+        [Sql("select r.[id], m.[date], m.[temp], m.[hum], r.[name], r.[id] as 'roomId', r.[name], r.[individual], r.[individual], rc.[targetTemp], rc.[targetHum] from measurements m "+
             "join rooms r on m.roomId=r.id join roomConfig rc on r.id=rc.roomId where date in (select max(date) from measurements group by roomId)",
-
             CommandType = System.Data.CommandType.Text,
             ConnectionStringSetting = "sqlconnectionstring")]
-        
-        IEnumerable<Room> room)
+        IEnumerable<Room> rooms)
         {
-            return new OkObjectResult(room);
+            var connection_str = Environment.GetEnvironmentVariable("sqldb_connection");
+
+            using (SqlConnection connection = new SqlConnection(connection_str))
+            {
+                foreach (var room in rooms)
+                {
+                    try
+                    {
+                        //  Raumkonfiguration basierend auf der Raum-ID abrufen + RoomConfig-Objekt erzeugen
+                        RoomConfig roomConfig = new RoomConfig(room.id, connection);
+                        if (roomConfig != null)
+                        {
+                            //  Ist-Raumtemperatur is kleiner als Soll-Temperatur
+                            if (room.temp.CompareTo(roomConfig.targetTemp + roomConfig.upperToleranceT) == -1)
+                            {
+                                room.status = "too low";
+                            } //  Ist-Raumtemperatur is groeﬂer als Soll-Temperatur
+                            else if (room.temp.CompareTo(roomConfig.targetTemp + roomConfig.upperToleranceT) == 1)
+                            {
+                                room.status = "too high";
+                            } //  Ist-Raumtemperatur is gleich als Soll-Temperatur
+                            else
+                            {
+                                room.status = "ok";
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        return new BadRequestObjectResult(ex);
+                    }
+                }                
+            }
+            return new OkObjectResult(rooms);
         }
     }
 }
