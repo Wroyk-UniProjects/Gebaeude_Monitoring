78ea1554f68685481e718432a279ec384275ab4c
Anscit06 <Schwmailandreas@gmail.com>
2022-05-29 19:16:00 +0200

    PSWTM-24 cs code added

---
+++ Model/Config.cs
@@ -0,0 +1,14 @@
+﻿namespace Building_Monitoring_WebApp.Model
+{
+    public class Config
+    {
+        public int Id { get; set; }
+        public double TargetHumi { get; set; }
+        public double UpToleHumi { get; set; }
+        public double LoToleHumi { get; set; }
+        public double TargetTemp { get; set; }
+        public double UpToleTemp { get; set; }
+        public double LoToleTemp { get; set; }
+        public double UpdateRate { get; set; }
+    }
+}
