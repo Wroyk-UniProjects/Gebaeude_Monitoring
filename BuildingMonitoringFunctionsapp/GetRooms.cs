71f4ffbbc5679f5384af84a58327127b3e538364
selina moritz <selinamm64@gmail.com>
2022-06-09 23:08:50 +0200

    PSWTM-31 rooms anzeigen, richtige targettemper anzeigen wenn global true

--- BuildingMonitoringFunctionsapp/GetRooms.cs
+++ BuildingMonitoringFunctionsapp/GetRooms.cs
@@ -14,8 +14,19 @@ namespace BuildingMonitoringFunctionsapp
         public static IActionResult Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "rooms")]
         HttpRequest req,
-        [Sql("select r.[id], m.[temper], m.[humid], r.[name], r.[global], rc.[targetTemper], rc.[targetHumid] from measurement m "+
-            "join room r on m.roomId=r.id join roomConfig rc on r.id=rc.id",
+        [Sql("(select r.[id], m.[temper], m.[humid], r.[name], r.[global], " +
+            "rc.[targetTemper], rc.[targetHumid] " +
+            "from measurement m  " +
+            "join room r on m.roomId=r.id " +
+            "join roomConfig rc on r.id=rc.id " +
+            "where r.global=0)"+
+            "union"+
+            "(select r.[id], m.[temper], m.[humid], " +
+            "r.[name], r.[global], " +
+            "(select rc1.targetTemper from roomConfig rc1 where rc1.id=0),  " +
+            "(select rc11.targetHumid from roomConfig rc11 where rc11.id=0) " +
+            "from measurement m  join room r on m.roomId=r.id " +
+            "join roomConfig rc on r.id=rc.id where r.global=1)",
             CommandType = System.Data.CommandType.Text,
             ConnectionStringSetting = "sqlconnectionstring")]
         IEnumerable<Room> rooms)
