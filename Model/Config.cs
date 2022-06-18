ba1730dd9d67b2f8f6cf88e5d3d9434a64910eba
Anscit06 <Schwmailandreas@gmail.com>
2022-06-18 15:30:41 +0200

    PSWTM-23 fixed Config naming of attributes

--- Model/Config.cs
+++ Model/Config.cs
@@ -3,12 +3,12 @@
     public class Config
     {
         public int Id { get; set; }
-        public double TargetHumi { get; set; }
-        public double UpToleHumi { get; set; }
-        public double LoToleHumi { get; set; }
-        public double TargetTemp { get; set; }
-        public double UpToleTemp { get; set; }
-        public double LoToleTemp { get; set; }
+        public double TargetTemper { get; set; }
+        public double TargetHumid { get; set; }
         public double UpdateRate { get; set; }
+        public double UpperToleranceTemper { get; set; }
+        public double LowerToleranceTemper { get; set; }
+        public double UpperToleranceHumid { get; set; }
+        public double LowerToleranceHumid { get; set; }
     }
 }
