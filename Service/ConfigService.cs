75c27d6d4f82a7d6d9562727bb572e8402d2e8c5
Rudolf Baun <rubait01@hs-esslingen.de>
2022-06-19 16:33:02 +0200

    PSWTM-72: extended Config Service to Put and Delete Individual Setings

--- Service/ConfigService.cs
+++ Service/ConfigService.cs
@@ -6,7 +6,7 @@ namespace Building_Monitoring_WebApp.Service
 {
     public class ConfigService : IConfigService
     {
-        private readonly string url = "https://building-monitoring.azurewebsites.net/api/rooms/config";
+        private readonly string urlBase = "https://building-monitoring.azurewebsites.net/api/rooms";
         private HttpClient client;
         private JsonSerializerOptions jsonSerializerOptions;
 
@@ -15,9 +15,18 @@ namespace Building_Monitoring_WebApp.Service
             this.client = client;
             this.jsonSerializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive  = true };
         }
-        public async Task<Config> GetConfig()
+
+		public async Task deleteIndvidualConfigByID(int id)
+		{
+            var response = await client.DeleteAsync(urlBase + $"/{id}/config/");
+
+            response.EnsureSuccessStatusCode();
+
+        }
+
+		public async Task<Config> GetConfig()
         {
-            var response = await client.GetAsync(url);
+            var response = await client.GetAsync(urlBase + "/config/");
 
             var content = await response.Content.ReadAsStringAsync();
 
@@ -29,11 +38,34 @@ namespace Building_Monitoring_WebApp.Service
             var config = JsonSerializer.Deserialize<Config>(content, jsonSerializerOptions);
             return config;
         }
-        public async Task PatchConfig(Config newConfig)
+
+		public async Task<Config> GetConfigByID(int id)
+		{
+            var response = await client.GetAsync(urlBase + $"/{id}/config/");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+
+            var config = JsonSerializer.Deserialize<Config>(content, jsonSerializerOptions);
+            return config;
+        }
+
+		public async Task PatchConfig(Config newConfig)
         {
             var jsonContent = JsonContent.Create(newConfig);
-            var response = await client.PatchAsync(url, jsonContent);
+            var response = await client.PatchAsync(urlBase + "/config/", jsonContent);
             response.EnsureSuccessStatusCode();
         }
-    }
+
+		public async Task putIndividualConfigByID(int id, Config config)
+		{
+            var jsonContent = JsonContent.Create(config);
+            var response = await client.PutAsync(urlBase + $"/{id}/config/", jsonContent);
+            response.EnsureSuccessStatusCode();
+        }
+	}
 }
