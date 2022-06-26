8bb817138f134107ffcc35f3f419bf8c27a46a1e
arleit00 <arleit00@hs-esslingen.de>
2022-06-27 01:34:58 +0200

    added wget

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+apt-get wget -y
+wget https://packages.microsoft.com/config/ubuntu/21.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 apt-get update; \
 apt-get install -y apt-transport-https && \
 apt-get update && \
