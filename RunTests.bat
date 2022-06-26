1ec0732a1df5da1e04ea8824f5560566a3b68c95
arleit00 <arleit00@hs-esslingen.de>
2022-06-27 01:00:31 +0200

    changed script

--- RunTests.bat
+++
@@ -1,6 +0,0 @@
-cd Building_Monitoring_WebApp
-start dotnet run 
-timeout /t 8
-cd ..
-cd Building_Monitoring_WebApp.Tests
-dotnet test
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install -y apt-transport-https && 
+sudo apt-get update && 
+sudo apt-get install -y dotnet-sdk-6.0
+sudo apt-get install unzip -y
