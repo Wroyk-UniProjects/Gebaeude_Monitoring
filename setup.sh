6439450f7bd27a593e8efeca0ee1e8ec0571e53d
arleit00 <arleit00@hs-esslingen.de>
2022-06-27 01:31:51 +0200

    added runtime

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-apt-get update
-apt-get install -y apt-transport-https && 
-apt-get update && 
+apt-get update; \
+apt-get install -y apt-transport-https && \
+apt-get update && \
 apt-get install -y dotnet-sdk-6.0
+apt-get update; \
+apt-get install -y apt-transport-https && \
+apt-get update && \
+apt-get install -y aspnetcore-runtime-6.0
 apt-get install unzip -y
 apt-get install curl -y
