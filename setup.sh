9822eee795eb01fb1c9f708984eef270d5a6e449
arleit00 <arleit00@hs-esslingen.de>
2022-06-27 02:19:09 +0200

    install dotnet with script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 apt-get wget -y
-wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb 
-apt install apt-transport-https -y
-apt update
-apt install dotnet-sdk-6.0 -y 
-apt install apt-transport-https -y
-apt update 
-apt install dotnet-runtime-6.0 -y
-dotnet --version
+wget https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c Current
 apt-get install unzip -y
 apt-get install curl -y
