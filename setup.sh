6d70edab2008973ea292b28505f6773059254aea
arleit00 <arleit00@hs-esslingen.de>
2022-06-27 02:25:01 +0200

    reverted script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 apt-get install wget -y
-wget https://dot.net/v1/dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh -c Current
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb 
+apt install apt-transport-https -y
+apt update
+apt install dotnet-sdk-6.0 -y 
+apt install apt-transport-https -y
+apt update 
+apt install dotnet-runtime-6.0 -y
+dotnet --version
 apt-get install unzip -y
 apt-get install curl -y
